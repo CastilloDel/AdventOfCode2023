493ced6c7ea8a72f5fd7a03e3b9c223df4abac9e
CastilloDel <delcastillodelarosadaniel@gmail.com>
2023-12-04 22:49:34 +0100

    Day3 second part

--- day3/src/Main.hs
+++ day3/src/Main.hs
@@ -4,11 +4,11 @@ module Main where
 
 import Data.Bifunctor (bimap)
 import Data.Char (isDigit)
-import qualified Data.HashSet as Set
 import Data.List (nub)
 import Data.Matrix (Matrix, ncols, nrows, safeGet, (!))
 import qualified Data.Matrix as Matrix
 import Data.Maybe (catMaybes, mapMaybe)
+import Debug.Trace (trace)
 
 main :: IO ()
 main = do
@@ -17,9 +17,12 @@ main = do
   putStrLn "First problem:"
   putStrLn $ "\tTest input: " ++ show (firstProblem testInput) ++ " == 4361"
   putStrLn $ "\tProblem input: " ++ show (firstProblem input) ++ " == 531561"
+  putStrLn "Second problem:"
+  putStrLn $ "\tTest input: " ++ show (secondProblem testInput) ++ " == 4361"
+  putStrLn $ "\tProblem input: " ++ show (secondProblem input) ++ " == 531561"
 
 firstProblem :: Matrix Char -> Int
-firstProblem a = sum $ map snd $ Set.elems $ Set.fromList numbersWithPosition
+firstProblem a = sum $ map snd $ nub numbersWithPosition
   where
     numbersWithPosition = map (`parseNumber` a) $ filter (`checkCloseToSymbol` a) numberIndexes
     numberIndexes = filter (\pos -> isDigit (a ! pos)) indexes
@@ -47,3 +50,17 @@ parseNumber (row, col) a = (pos, number)
     safeGetNumber row col a = case safeGet row col a of
       Just n -> if isDigit n then Just n else Nothing
       Nothing -> Nothing
+
+secondProblem :: Matrix Char -> Int
+secondProblem a = sum $ map product gearNumbers
+  where
+    gearNumbers = filter ((== 2) . length) $ map (map snd . nub) possibleGearNumbers
+    possibleGearNumbers = map (map (`parseNumber` a) . (`getSurroundingNumberIndexes` a)) possibleGearIndexes
+    possibleGearIndexes = filter (\pos -> '*' == (a ! pos)) indexes
+    indexes = concatMap (\i -> map (i,) [1 .. (ncols a)]) [1 .. nrows a]
+
+getSurroundingNumberIndexes :: (Int, Int) -> Matrix Char -> [(Int, Int)]
+getSurroundingNumberIndexes pos a = filter (\i -> isDigit (a ! i)) possibleIndexes
+  where
+    possibleIndexes = map (\offset -> bimap (fst offset +) (snd offset +) pos) offsets
+    offsets = concatMap (filter (/= (0, 0)) . zip [-1, 0, 1] . replicate 3) [-1, 0, 1]
