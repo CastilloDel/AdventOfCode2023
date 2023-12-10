c95486923a6ccda9f166f517a7cadb54e58e1579
CastilloDel <delcastillodelarosadaniel@gmail.com>
2023-12-10 13:06:44 +0100

    Day5 second part

--- day5/src/Main.hs
+++ day5/src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Data.Char (isDigit)
+import Data.List (find)
 import Text.ParserCombinators.ReadP (ReadP, char, many1, readP_to_S, readS_to_P, satisfy, sepBy1, string)
 
 main :: IO ()
@@ -9,7 +10,10 @@ main = do
   input <- readInput <$> readFile "day5/input"
   putStrLn "First problem:"
   putStrLn $ "\tTest input: " ++ show (firstProblem testInput) ++ " == 35"
-  putStrLn $ "\tProblem input: " ++ show (firstProblem input) ++ " == 20667"
+  putStrLn $ "\tProblem input: " ++ show (firstProblem input) ++ " == 510109797"
+  putStrLn "Second problem:"
+  putStrLn $ "\tTest input: " ++ show (secondProblem testInput) ++ " == 46"
+  putStrLn $ "\tProblem input: " ++ show (secondProblem input) ++ " == 9622622"
   where
     readInput = fst . last . readP_to_S parseProblemData
 
@@ -56,3 +60,26 @@ applyMapping start mapping
   where
     possibleMatch = filter (\(_, source, len) -> start >= source && start <= source + len) mapping
     calculateDest (dest, source, _) n = n - source + dest
+
+secondProblem :: ProblemData -> Int
+secondProblem (ProblemData seeds mappings) = minimum $ map fst $ applyMappings' seedRanges mappings
+  where
+    seedRanges = getSeedRanges seeds
+    getSeedRanges (a : b : c) = (a, b) : getSeedRanges c
+    getSeedRanges [] = []
+
+applyMappings' :: [(Int, Int)] -> [Mapping] -> [(Int, Int)]
+applyMappings' = foldl applyMapping'
+
+applyMapping' :: [(Int, Int)] -> Mapping -> [(Int, Int)]
+applyMapping' ranges mapping = concatMap (`applyMappingToRange` mapping) ranges
+
+applyMappingToRange :: (Int, Int) -> Mapping -> [(Int, Int)]
+applyMappingToRange (start, len) mapping = maybe [(start, len)] exploreMatch firstMatch
+  where
+    firstMatch = find (\(dest, source, matchLen) -> start + len > source && start < source + matchLen) mapping
+    exploreMatch (dest, source, matchLen)
+      | start < source && start + len <= source + matchLen = (dest, len - (source - start)) : applyMappingToRange (start, source - start) mapping
+      | start >= source && start + len > source + matchLen = (dest + (start - source), matchLen - (start - source)) : applyMappingToRange (source + matchLen + 1, matchLen - (start - source)) mapping
+      | start < source && start + len > source + matchLen = (dest, matchLen) : applyMappingToRange (start, source - start) mapping ++ applyMappingToRange (source + matchLen + 1, len - matchLen - (source - start)) mapping
+      | otherwise = [(dest + (start - source), len)]
